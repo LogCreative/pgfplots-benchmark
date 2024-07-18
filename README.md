aa4fc45b063a7f29eaa19ae19cd9a50f2b38d1d3
LogCreative <logcreative@outlook.com>
2024-07-18 19:42:01 +0800

    feat: visualize the result

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# PGFPlotsEdt-benchmark
-Benchmark PGFPlotsEdt and other platforms
+# PGFPlots-benchmark
+Benchmark LaTeX compilation platforms based on PGFPlots examples
 
 
 ```bash
 conda env update -n ppedt_benchmark -f ppedt_benchmark.yml
 conda activate ppedt_benchmark
 ```
+
+![](figs/avg.png)
+
+![](figs/full.png)
--- merge_result.py
+++ merge_result.py
@@ -1,20 +1,24 @@
 import pandas as pd
+import numpy as np
 
 def read_csv_result(csv_file, platform):
-    return pd.read_csv(csv_file, index_col="file")["time"].dropna().rename(platform)
+    return pd.read_csv(csv_file, index_col="file")["time"].rename(platform)
 
-result_ppedt = read_csv_result("ppedt_times.csv", "ppedt")
-result_ppedt_deploy = read_csv_result("ppedt_deploy_times.csv", "ppedt_deploy")
-result_laton = read_csv_result("laton_times.csv", "laton")
+result_ppedt = read_csv_result("ppedt_times.csv", "PGFPlotsEdt")
+result_ppedt_deploy = read_csv_result("ppedt_deploy_times.csv", "PGFPlotsEdt-deploy")
+result_laton = read_csv_result("laton_times.csv", "LaTeXOnline")
 
 # Only keep the common successful files
-merged_result = pd.DataFrame([result_ppedt, result_ppedt_deploy, result_laton]).T.dropna()
+merged_result = pd.DataFrame([result_ppedt_deploy, result_ppedt, result_laton]).T
 
 # index column only keep the parsed number
 merged_result.index = merged_result.index.str.extract(r"(\d+)", expand=False).astype(int)
 
 # save the full result
-merged_result.to_csv("merged_result_times.csv")
+merged_result.replace(np.nan, 0).to_csv("merged_result_times.csv")
+
+# drop the NaN
+merged_result = merged_result.dropna()
 
 # save the average time for each column
-merged_result.mean().to_csv("merged_result_times_avg.csv", header=["avg_time"], index_label="method")
+merged_result.mean(skipna=True).to_csv("merged_result_times_avg.csv", header=["avg"], index_label="method")
